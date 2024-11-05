bf4e369f2c7168a19773758c950427d2a70a9421
pedrobicas <pedrobicascouto@hotmail.com>
2024-11-04 22:40:35 -0300

    table style

--- src/app/component/students/students.component.ts
+++ src/app/component/students/students.component.ts
@@ -67,100 +67,60 @@ export class StudentsComponent implements AfterViewInit {
       priority: 1, 
       progressing: 3, 
       mastering: 6,
-      avatar: 'https://avataaars.io/?avatarStyle=Circle&topType=ShortHairDreads01&accessoriesType=Wayfarers&hairColor=Black&facialHairType=Blank&clotheType=Blazer&eyeType=Happy&eyebrowType=Default&mouthType=Smile&skinColor=Light'
+      avatar: 'https://avataaars.io/?avatarStyle=Circle&topType=ShortHairDreads01&accessoriesType=Blank&hairColor=Black&facialHairType=Blank&clotheType=BlazerShirt&eyeType=Happy&eyebrowType=Default&mouthType=Smile&skinColor=Light'
     },
     { 
-      name: 'Lucas Mendes', 
-      email: 'lucas@example.com', 
+      name: 'Pedro Almeida', 
+      email: 'pedro@example.com', 
       completedTrainings: '5/10', 
       class: 'B', 
-      averageGrade: 4.5, 
-      priority: 2, 
-      progressing: 4, 
-      mastering: 3,
-      avatar: 'https://avataaars.io/?avatarStyle=Circle&topType=ShortHairShortCurly&accessoriesType=Round&hairColor=Brown&facialHairType=BeardMedium&clotheType=Hoodie&eyeType=Happy&eyebrowType=Default&mouthType=Smile&skinColor=Light'
-    },
-    { 
-      name: 'Ana Clara', 
-      email: 'anaclara@example.com', 
-      completedTrainings: '8/10', 
-      class: 'B', 
-      averageGrade: 9.2, 
-      priority: 1, 
-      progressing: 1, 
-      mastering: 7,
-      avatar: 'https://avataaars.io/?avatarStyle=Circle&topType=ShortHairDreads01&accessoriesType=Round&hairColor=Black&facialHairType=BeardMedium&clotheType=Hoodie&eyeType=Happy&eyebrowType=Default&mouthType=Smile&skinColor=Light'
-    },
-    { 
-      name: 'Pedro Silva', 
-      email: 'pedro@example.com', 
-      completedTrainings: '6/10', 
-      class: 'C', 
-      averageGrade: 7.5, 
-      priority: 3, 
-      progressing: 2, 
-      mastering: 5,
-      avatar: 'https://avataaars.io/?avatarStyle=Circle&topType=LongHairStraight&accessoriesType=Round&hairColor=Brown&facialHairType=Blank&clotheType=Shirt&eyeType=Happy&eyebrowType=Default&mouthType=Smile&skinColor=Light'
-    },
-    { 
-      name: 'Bianca Pereira', 
-      email: 'bianca@example.com', 
-      completedTrainings: '4/10', 
-      class: 'C', 
-      averageGrade: 5.8, 
-      priority: 4, 
+      averageGrade: 4.2, 
+      priority: 6, 
       progressing: 3, 
       mastering: 2,
-      avatar: 'https://avataaars.io/?avatarStyle=Circle&topType=LongHairCurly&accessoriesType=Round&hairColor=Brown&facialHairType=Blank&clotheType=Hoodie&eyeType=Happy&eyebrowType=Default&mouthType=Smile&skinColor=Light'
+      avatar: 'https://avataaars.io/?avatarStyle=Circle&topType=LongHairShort&accessoriesType=Wayfarers&hairColor=Brown&facialHairType=Blank&clotheType=Shirt&eyeType=Happy&eyebrowType=Default&mouthType=Smile&skinColor=Light'
     },
     { 
-      name: 'Gabriel Lima', 
-      email: 'gabriel@example.com', 
-      completedTrainings: '9/10', 
-      class: 'D', 
-      averageGrade: 8.0, 
+      name: 'Ana Lima', 
+      email: 'ana@example.com', 
+      completedTrainings: '8/10', 
+      class: 'B', 
+      averageGrade: 9.5, 
       priority: 2, 
-      progressing: 5, 
+      progressing: 6, 
+      mastering: 4,
+      avatar: 'https://avataaars.io/?avatarStyle=Circle&topType=LongHairCurly&accessoriesType=Round&hairColor=Black&facialHairType=Blank&clotheType=Shirt&eyeType=Happy&eyebrowType=Default&mouthType=Smile&skinColor=Light'
+    },
+    { 
+      name: 'Lucas Ferreira', 
+      email: 'lucas@example.com', 
+      completedTrainings: '6/10', 
+      class: 'C', 
+      averageGrade: 5.0, 
+      priority: 4, 
+      progressing: 4, 
       mastering: 3,
-      avatar: 'https://avataaars.io/?avatarStyle=Circle&topType=LongHairStraight&accessoriesType=Round&hairColor=Blonde&facialHairType=BeardMedium&clotheType=Hoodie&eyeType=Happy&eyebrowType=Default&mouthType=Smile&skinColor=Light'
+      avatar: 'https://avataaars.io/?avatarStyle=Circle&topType=LongHairShort&accessoriesType=Round&hairColor=Brown&facialHairType=Blank&clotheType=Shirt&eyeType=Happy&eyebrowType=Default&mouthType=Smile&skinColor=Light'
     },
     { 
       name: 'Fernanda Costa', 
       email: 'fernanda@example.com', 
-      completedTrainings: '2/10', 
-      class: 'D', 
-      averageGrade: 3.9, 
+      completedTrainings: '9/10', 
+      class: 'C', 
+      averageGrade: 7.5, 
       priority: 5, 
-      progressing: 1, 
-      mastering: 1,
-      avatar: 'https://avataaars.io/?avatarStyle=Circle&topType=LongHairStraight&accessoriesType=Round&hairColor=Black&facialHairType=BeardMedium&clotheType=Shirt&eyeType=Happy&eyebrowType=Default&mouthType=Smile&skinColor=Light'
+      progressing: 5, 
+      mastering: 2,
+      avatar: 'https://avataaars.io/?avatarStyle=Circle&topType=LongHairStraight&accessoriesType=Prescription02&hairColor=Brown&facialHairType=Blank&clotheType=Hoodie&eyeType=Happy&eyebrowType=Default&mouthType=Smile&skinColor=Light'
     }
+    // Adicione mais alunos conforme necessário
   ];
 
+  displayedColumns: string[] = ['name', 'completedTrainings', 'averageGrade', 'class', 'priority', 'progressing', 'mastering'];
   studentsDataSource = new MatTableDataSource<Student>(this.students);
-  displayedColumns: string[] = ['avatar', 'name', 'completedTrainings', 'averageGrade', 'class', 'priority', 'progressing', 'mastering'];
-
-  constructor() {}
 
   ngAfterViewInit() {
     this.studentsDataSource.paginator = this.paginator;
     this.studentsDataSource.sort = this.sort;
   }
-
-  applyFilter(event: Event) {
-    const filterValue = (event.target as HTMLInputElement).value;
-    this.studentsDataSource.filter = filterValue.trim().toLowerCase();
-  }
-
-  getBarColor(averageGrade: number): string {
-    if (averageGrade < 5) {
-      return 'red-bar';
-    } else if (averageGrade >= 5 && averageGrade < 8) {
-      return 'yellow-bar';
-    } else {
-      return 'green-bar';
-    }
-  }
-  
-  
 }
